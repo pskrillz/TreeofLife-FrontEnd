adbfafc81fec56a72ac54432f6db1bb3aca299cd
pi <payam.farah@live.com>
2021-06-07 20:10:14 -0700

    toolbar implemented successfully

--- TreeOfLife/src/app/app.module.ts
+++ TreeOfLife/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from '../app/Components/toolbar/toolbar.component'
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
